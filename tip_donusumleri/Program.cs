9ea69ce203f7ad756a5135558023608ab130ef6a
Rüstem Muhammed KARADEMİR <rustemkarademir@gmail.com>
2021-12-17 16:15:34 +0300

    .

---
+++ tip_donusumleri/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace tip_donusumleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            float y = x;
+            System.Console.WriteLine(y);
+
+        }
+    }
+}
