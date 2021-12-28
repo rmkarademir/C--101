5ec7d2ac6200b791096fe719f9cbe53fe4b3b5ab
Rüstem Muhammed KARADEMİR <rustemkarademir@gmail.com>
2021-12-28 21:51:44 +0300

    metotlar

---
+++ metotlar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sonuc = Topla(5,4);
+            int x=5,y=4;
+            Metot metot = new Metot();
+            metot.Yazdır("Toplam = "+sonuc.ToString());
+            metot.Yazdır("Toplam = "+metot.degiskenDegistir(ref x, ref y).ToString());
+            Console.WriteLine("x= "+x +" y= "+ y);
+        }
+
+        static int Topla(int sayi1, int sayi2){
+            return sayi1+sayi2;
+        }
+    }
+    class Metot
+    {
+        public void Yazdır(string str){
+            Console.WriteLine(str);
+        }
+        public int degiskenDegistir(ref int sayi1, ref int sayi2){
+            sayi1 +=10;
+            sayi2 +=10;
+            return sayi1+sayi2;
+        }
+    }
+}
